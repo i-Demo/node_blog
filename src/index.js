b6b0a9ad18e35be9837178ec75d0b89057d06b23
Hoai Vi <hoaivi.it@gmail.com>
2023-02-28 17:21:48 +0700

    Test

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Template engine
 app.engine(
     'hbs',
-                 handlebars.engine({
-                  extname: '.hbs',
-         }),
+    handlebars.engine({
+        extname: '.hbs',
+    }),
 );
 
 app.set('view engine', 'hbs');
@@ -27,4 +27,4 @@ app.set('views', path.join(__dirname, 'resources/views'));
 // Routes init
 route(app);
 
-                app.listen(port, () => console.log(`App listening at localhost:${port}`));
+app.listen(port, () => console.log(`App listening at localhost:${port}`));
